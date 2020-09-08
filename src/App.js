fc3648c18b1054e0a7372936a83407f8b2d88e8d
Vlad <boi.foma@yandex.ru>
2020-09-08 16:06:19 +0500

    Checkbox state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 import SearchBar from "./components/SearchBar";
 import TableProducts from "./components/TableProducts";
 
-const App = () => {
-	return (
-		<div className="App">
-			<SearchBar />
-			<TableProducts />
-		</div>
-	);
-};
+class App extends Component {
+	state = {
+		isStocked: false
+	};
+
+	handleCheckbox = (isStocked) => {
+		this.setState({
+			isStocked
+		});
+	};
+
+	render() {
+		const { isStocked } = this.state;
+
+		return (
+			<div className="App">
+				<SearchBar
+					inStock={isStocked}
+					onCheckboxChange={this.handleCheckbox}
+				/>
+				<TableProducts />
+			</div>
+		);
+	}
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+	handleCheckbox = (e) => {
+		this.props.onCheckboxChange(e.target.checked);
+	};
+
 	render() {
+		const { inStock } = this.props;
 		return (
 			<div>
 				<input type="text" placeholder="Search..." /> <br />
-				<input type="checkbox" /> Only show products in stock
+				<input
+					type="checkbox"
+					checked={inStock}
+					onChange={this.handleCheckbox}
+				/>{" "}
+				Only show products in stock
 			</div>
 		);
 	}
